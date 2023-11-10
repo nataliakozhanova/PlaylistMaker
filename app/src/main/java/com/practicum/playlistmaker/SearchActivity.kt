aa6e7c60adc92b70cffe9f7ddbb2526317a414e2
nataliakozhanova <Natalia.v.kozhanova@gmail.com>
2023-11-10 18:50:36 +0100

    Спринт 9. ДЗ

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,93 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        const val TEXT_TO_SAVE = "TEXT_TO_SAVE"
+        const val TEXT_TO_SAVE_DEF = ""
+    }
+
+    private var textToSave: String = TEXT_TO_SAVE_DEF
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_TO_SAVE, textToSave)
+        //Log.d("search","saved")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textToSave = savedInstanceState.getString(TEXT_TO_SAVE, TEXT_TO_SAVE_DEF)
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        inputEditText.setText(textToSave)
+        //Log.d("search","restored")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val searchArrowBackButton = findViewById<Toolbar>(R.id.toolbar)
+
+        searchArrowBackButton.setNavigationOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.clear_button)
+
+         val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(inputEditText.editableText)
+                textToSave = s.toString()
+
+                //Log.d("search","ping")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+
+        }
+            inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        clearButton.setOnClickListener {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+            inputEditText.setText("")
+            inputEditText.isCursorVisible = false
+            clearButton.visibility = View.GONE
+        }
+    }
+
+
+
+
+    private fun clearButtonVisibility(editable: Editable?): Int {
+        return if (editable.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,21 +2,47 @@ package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import com.practicum.playlistmaker.R.id.settings_arrow_back_button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class SettingsActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val settingsArrowBackButton = findViewById<ImageView>(R.id.settings_arrow_back_button)
-        settingsArrowBackButton.setOnClickListener {
+        val settingsArrowBackButton = findViewById<Toolbar>(R.id.settings_toolbar)
+        settingsArrowBackButton.setNavigationOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
+        val shareAppButton = findViewById<ImageView>(R.id.share_app_button)
+        shareAppButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.link_share_app))
+            startActivity(Intent.createChooser(shareIntent, ""))
+        }
+        val writeToSupportButton = findViewById<ImageView>(R.id.write_to_support_button)
+        writeToSupportButton.setOnClickListener {
+            val writeIntent = Intent(Intent.ACTION_SENDTO)
+            writeIntent.data = Uri.parse("mailto:" + getString(R.string.to))
+            //writeIntent.putExtra(Intent.EXTRA_EMAIL, getString(R.string.to))
+            writeIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject))
+            writeIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message))
+            startActivity(writeIntent)
+        }
+        val termsOfUseButton = findViewById<ImageView>(R.id.terms_of_use_button)
+        termsOfUseButton.setOnClickListener {
+            val browseIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_terms_of_usage)))
+            startActivity(browseIntent)
+        }
+
     }
+
 }
